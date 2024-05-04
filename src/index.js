add9b04a4d8fdeef2d17d7d9c1f5ea93e79f13ca
shivam yadav <91057727+Shivamycodee@users.noreply.github.com>
2024-05-04 10:56:08 +0530

    images changed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from './app/App';
 import './index.css';
 
+// Google Analytics tracking code
+const trackingCode = `
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-KRTHN0EN7N"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-KRTHN0EN7N');
+  </script>
+`;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <>
+    <div dangerouslySetInnerHTML={{ __html: trackingCode }} />
     <App />
+  </>
 );
