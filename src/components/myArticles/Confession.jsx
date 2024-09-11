573baa6cf9c2d1887410d5f593261159e42bf238
Shivamycodee <91057727+Shivamycodee@users.noreply.github.com>
2024-09-11 16:44:31 +0530

    article update

--- src/components/myArticles/Confession.jsx
+++ src/components/myArticles/Confession.jsx
@@ -3,6 +3,9 @@ import "../myArticles/style.css";
 import FirstConf from '../myArticles/articles/first-conf.png'
 import SecondConf from '../myArticles/articles/second-conf.png'
 import ThirdConf from '../myArticles/articles/third-conf.png'
+import FourConf from '../myArticles/articles/four-conf.png'
+import FiveConf from '../myArticles/articles/five-conf.png'
+import SixConf from '../myArticles/articles/six-conf.png'
 
 
 function Confession() {
@@ -29,30 +32,62 @@ function Confession() {
 <p>&nbsp;</p>
 
 <ol>
-<li>As you can see in the image below i'm running my webiste , which is sending a post request to the server at endpoint&nbsp;checkData, i'm sending a JWT Bearer token and encrypted payload with my request.<br /><br /><br /><br />
+<li>As you can see in the image below i'm running my webiste, which is sending a post request to the server at endpoint checkData, i'm sending a JWT Bearer token and encrypted payload with my request.<br /><br /><br /><br />
 <img src={FirstConf} alt="" width="100%" height="500" />
-<img src={SecondConf} alt="" width="100%" height="100" style={{marginTop:20}} />
+<img src={SecondConf} alt="" width="100%" height="100" />
 </li>
 
-<p>&nbsp;</p>
-<p>&nbsp;</p>
 
-<li>The issue with the current procedure i took (and many people do this too) that i assumed that using JWT with short expiry time (mine is 30 seconds) and sending encrypted paylaod is enough to protect your server from replay attack, DOS attack, DDOS attack , etc... WHICH is not. here in the below image i'm doing a replay attack using a third party POSTMAN DESKTOP , and able to easily fetch/manipulate my data by
-  simply taking the JWT (before it expires) and the encrypted payload and enter it in postman.<br /><br /><br />
+<li>The issue with the current procedure I followed (and many others do as well) is that I assumed using JWTs with short expiration times (mine is 30 seconds) and sending encrypted payloads would be enough to protect my server from replay attacks, 
+DoS attacks, DDoS attacks, etc. However, it is not. In the image below, I demonstrate a replay attack using a third-party tool,
+Postman Desktop, and I was able to easily fetch and manipulate my data by simply taking the JWT (before it expired) and the encrypted payload, then entering them into Postman.<br /><br /><br />
 <img src={ThirdConf} alt="" width="100%" height="700" style={{marginBottom:20}} />
 
-Now you will say but what is i keep the expire time of my JWT too short like new few seconds, although client side had to call it multiple times but the hacker won't be able to do anything , right? NO. Your JWT endpoint would be open for anyone to call so hacker can just call for a new JWT and use the OLD encrypted
-payload to get the data.
+Now you might say, 'But what if I keep the expiration time of my JWT very short, like just a few seconds? Although the client would need to request it multiple times, the hacker wouldn’t be able to do anything, right?' Wrong. Your JWT endpoint would still be open for anyone to call, so a hacker could simply request a new JWT and use the old encrypted payload to access the data.
 </li>
 </ol>
 
-        <p>&nbsp;</p>
+<p>My package, <a href='https://www.npmjs.com/package/@shivamycodee/confession' target='_blank'>@shivamycodee/confession</a>, resolves this issue. It uses <a href='https://bun.sh' target='_blank'>Bun</a> to make it optimized and fast.</p>
 
-<p>My package @shivamycodee/confession resolves this issue, and my packge is using <a style={{ color: "lightblue" }} href='https://bun.sh' target="_blank" >bun</a> to make it optamized and fast.
+<h2 style={{marginTop:50,marginBottom:50}} >How package handles this issue?</h2>
+
+<p>
+My package uses the encrypted payload as a key to differentiate between requests, and I have added a nonce to the encrypted payload to keep track of each request. I store every request in a cache folder with a timestamp. The request will be removed from the server after the JWT expiration time.
+For example, if the JWT expiration time is 30 seconds, any request hash older than 30 seconds will be removed from the server. So, if a hacker attempts a replay attack using a valid JWT and encrypted payload, this is the response from my package.
+But what if the hacker uses a valid JWT and an old encrypted payload that has been removed from the server cache? Don’t worry—those cases are also handled by my package. Below, I have demonstrated various attacks and the responses from my package.
+<p>&nbsp;</p>
+<p style={{color:"#a85454"}} >HERE THE HACKER I'M REFEREING TO CAN BY A THIRD PARTY BETWEEN CLIENT AND SERVER OR THE CLIENT HIMSELF.</p>
 </p>
+<p>&nbsp;</p>
+
+<ol>
+
+<li>  When hacker tries to do a replay attack using a valid JWT and encrypted payload, this would be the response from my package:
+  <img src={FourConf} alt="" width="100%" height="600" />
+"Replay attack detected" would be returned to the request.</li>
+
+  <li>  When hacker tries to do a replay attack using an expired JWT and encrypted payload, this would be the response from my package:
+  <img src={FiveConf} alt="" width="100%" height="600" />
+</li>
+
+<li> When hacker tries to do a replay attack using a valid JWT and an OLD encrypted payload which is removed from the server cache, this would be the response from my package:
+    <img src={SixConf} alt="" width="100%" height="600" />
+   "Hacker using old EncryptedPayload Hash." would be returned to the request.
+</li>
+</ol>
+
+<p>&nbsp;</p>
+<p>This way, confession package handles and prevent your server from Hacker</p>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 
 
-<p>If you find any problem/issue in my package, please create an <a style={{ color: "lightblue" }} href="https://github.com/Shivamycodee/confession/issues/new" target="_blank" >issue thread here.</a></p>
+<p>If you find any problem/issue in my package, please create an <a href="https://github.com/Shivamycodee/confession/issues/new" target="_blank" >issue thread here.</a></p>
+
        
 <strong>
               If you are interested in this idea, hit me up at
